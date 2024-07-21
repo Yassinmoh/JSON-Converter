bd804eadcdc15620c1bb6a9800bc92040b8a649c
Yassinmoh <yassin_mohamed310.com>
2024-07-22 01:39:39 +0300

    Feat(Login): create login form

--- src/app/modules/core/theme/login-page/login-page.component.ts
+++ src/app/modules/core/theme/login-page/login-page.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { PasswordModule } from 'primeng/password';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule,CommonModule,PasswordModule,IconFieldModule,InputIconModule,InputTextModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit{
 
+  loginForm!: FormGroup;
+
+  _fb = inject(FormBuilder)
+
+  ngOnInit(): void {
+    this.loginForm = this._fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
