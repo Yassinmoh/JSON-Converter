d0814cdfe6902d35846612bf421253fbd160b462
ymohamed <ymohamed@sarmady.net>
2024-07-21 18:04:34 +0300

    Feat(Home): Update json data table

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule,TableModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
+
+
+
 export class HomePageComponent implements OnInit {
 
   jsonForm!: FormGroup;
@@ -18,8 +22,18 @@ export class HomePageComponent implements OnInit {
 
   ngOnInit(): void {
     this.jsonForm = this._fb.group({
-      jsonInput: ['', [Validators.required, this.jsonValidator.bind(this)]]
+      jsonInput: ['', [Validators.required,this.jsonValidator.bind(this)]]
     });
+
+    this.jsonForm.get('jsonInput')?.valueChanges.subscribe(value => {
+      try {
+        this.jsonData = JSON.parse(value);
+        this.errorMessage = null;
+      } catch (e) {
+        this.jsonData = null;
+      }
+    });
+
   }
 
   jsonValidator(control: any) {
@@ -31,5 +45,9 @@ export class HomePageComponent implements OnInit {
       this.errorMessage = 'Invalid JSON format';
       return { invalidJson: true };
     }
+  }
+
+  get jsonKeys(): string[] {
+    return  Object.keys(this.jsonData[0]);
   }
 }
